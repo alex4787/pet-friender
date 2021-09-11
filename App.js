97ee315f974eab7fd686284a9de6b5519365aa96
Alex Chan <alex.chan@shopify.com>
2021-09-10 20:51:51 -0400

    Added react-navigator

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
+const HomeScreen = () => {
+	return (
+		<View style={styles.container}>
+			<Text>Hello world</Text>
+		</View>
+	)
+};
 
 const styles = StyleSheet.create({
   container: {
