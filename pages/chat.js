5b49f0fd5d2ceabba73adafa750c20b819d5caa3
Alex Chan <alex.chan@shopify.com>
2021-09-11 11:50:42 -0400

    Chat stuff

--- pages/chat.js
+++ pages/chat.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements'
+import { ChatScreen } from './chatScreen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 
-export const Chat = ({ list }) =>{
+export const Chat = ({ navigation, list }) =>{
   return (
-    <View>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="ChatList" children={props => <ChatList list={list} {...props} />} />
+      {
+        list.map(match => (
+          <Stack.Screen name={match.name} children={props => <ChatScreen name={match.name} {...props} /> } />
+        ))
+      }
+    </Stack.Navigator>
+  )
+}
+
+const ChatList = ({ navigation, list }) => (
+  <View>
     {
       list.map((match, i) => (
-        <ListItem key={i} bottomDivider>
+        <ListItem key={i} bottomDivider onPress={() => navigation.navigate(match.name)}>
           <Avatar source={{uri: match.avatar_url}} />
           <ListItem.Content>
             <ListItem.Title>{match.name}'s {match.dog_name}</ListItem.Title>
@@ -18,5 +33,4 @@ export const Chat = ({ list }) =>{
       ))
     }
   </View>
-  )
-}
+)
