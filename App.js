bef22f2ae3243130b6d9ca8a3b8db69b1c82ba7f
LAPTOP-UJOEU1DN\lomta <mariamilomtadze@cmail.carleton.ca>
2021-09-10 21:26:19 -0400

    login

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -12,7 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
+          component={Login}
           options={{ title: 'Welcome' }}
         />
       </Stack.Navigator>
@@ -27,6 +27,35 @@ const HomeScreen = () => {
 		</View>
 	)
 };
+
+const Login = () => {
+  const [email, onChangeEmail] = React.useState(null);
+  const [password, onChangePassword] = React.useState(null);
+
+  return(
+      <SafeAreaView>
+          <TextInput 
+              placeholder="Email"
+              onChangeText={onChangeEmail}
+              value={email}
+          />
+          <TextInput
+               secureTextEntry={true}
+               placeholder="Password"
+               onChangeText={onChangePassword}
+               value={password}
+          />
+          <Button
+              title="Log In"
+          />
+
+          <TouchableOpacity>
+            <Text>Don't you have an account? Click here to</Text>
+          </TouchableOpacity>
+   
+      </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
